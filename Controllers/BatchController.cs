dfd375cb5ee68bede8a128a87424ae49c84dae7a
sayedbintaher <sayedbintaher@email.com>
2023-02-24 11:54:03 +0600

    Batch CRUD Added and some few changes made

--- Controllers/BatchController.cs
+++ Controllers/BatchController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using CourseManagementSystem.ViewModels.BatchVM;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SchoolManagementSystem.Data;
+using SchoolManagementSystem.Models;
+using SchoolManagementSystem.ViewModels.BatchVM;
+using SchoolManagementSystem.ViewModels.CourseVM;
 
 namespace SchoolManagementSystem.Controllers
 {
@@ -19,8 +23,101 @@ namespace SchoolManagementSystem.Controllers
             return View(batches);
         }
 
-        public IActionResult AddBatch()
+        public IActionResult CreateBatch()
         {
+            return View(new CreateBatchVM());
+        }
+
+        [HttpPost]
+        public IActionResult CreateBatch(CreateBatchVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                var batch = new Batch()
+                {
+                    BatchCode= model.BatchCode,
+                    Year=model.Year
+                };
+                _db.Batches.Add(batch);
+                if(_db.SaveChanges() > 0)
+                {
+                    RedirectToAction("GetBatches");
+                }
+            }
+            return View();
+        }
+        public IActionResult EditBatch(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var batch = _db.Batches.FirstOrDefault(b => b.Id == id);
+            if (batch == null)
+            {
+                return NotFound();
+            }
+            var batchVM = new EditBatchVM()
+            {
+                Id = batch.Id,
+                BatchCode = batch.BatchCode,
+                Year = batch.Year
+            };
+            return View(batchVM);
+
+        }
+        [HttpPost]
+        public IActionResult EditBatch(EditBatchVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                var batch = new Batch()
+                {
+                    Id = model.Id,
+                    BatchCode = model.BatchCode,
+                    Year = model.Year
+                };
+                _db.Batches.Update(batch);
+                if (_db.SaveChanges() > 0)
+                {
+                    return RedirectToAction("GetBatches");
+                }
+            }
+            return View();
+        }
+        public IActionResult DeleteBatch(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var course = _db.Batches.FirstOrDefault(b => b.Id == id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+            var courseVM = new DeleteBatchVM()
+            {
+                Id = course.Id,
+                BatchCode = course.BatchCode,
+                Year = course.Year
+            };
+            return View(courseVM);
+        }
+        [HttpPost]
+        public IActionResult DeleteBatch(DeleteBatchVM model)
+        {
+            var obj = _db.Batches.FirstOrDefault(b => b.Id == model.Id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Batches.Remove(obj);
+            if (_db.SaveChanges() > 0)
+            {
+                return RedirectToAction("GetBatches");
+            }
+
             return View();
         }
     }
---
+++ ViewModels/BatchVM/DeleteBatchVM.cs
@@ -0,0 +1,9 @@
+﻿namespace CourseManagementSystem.ViewModels.BatchVM
+{
+    public class DeleteBatchVM
+    {
+        public int Id { get; set; }
+        public string BatchCode { get; set; }
+        public string Year { get; set; }
+    }
+}
